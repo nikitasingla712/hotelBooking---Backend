b17441018b13b0d65c2d0c798d23c0bcba54a29e
nikitasingla712 <nikitasingla712@gmail.com>
2020-03-17 19:02:23 +0530

    backend

--- src/main/java/com/example/springmongodemo/controller/PersonController.java
+++ src/main/java/com/example/springmongodemo/controller/PersonController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-@RestController
 @CrossOrigin(origins = "*")
+@RestController
+
 public class PersonController {
 
     @Autowired
