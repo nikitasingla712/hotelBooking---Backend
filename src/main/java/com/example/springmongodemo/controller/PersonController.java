2aec0c7b9fc32aaa26979606ea1126b369768fad
nikitasingla712 <nikitasingla712@gmail.com>
2020-03-17 19:49:27 +0530

    backend

--- src/main/java/com/example/springmongodemo/controller/PersonController.java
+++ src/main/java/com/example/springmongodemo/controller/PersonController.java
@@ -3,17 +3,8 @@ package com.example.springmongodemo.controller;
 import com.example.springmongodemo.model.Person;
 import com.example.springmongodemo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 @CrossOrigin(origins = "*")
 @RestController
